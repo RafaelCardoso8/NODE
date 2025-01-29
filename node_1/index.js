191b6c60b1cc0480c90cab8ee5e9f363ef833365
Rafael Cardoso <faelcardoso8@gmail.com>
2025-01-28 21:13:00 -0300

    feat
    Meu primeiro servidor

--- node_1/index.js
+++ node_1/index.js
@@ -1,1 +1,22 @@
 const express = require("express")
+
+const servidor = express()
+
+//rotas
+
+servidor.get("/Cavaleiro", (resquest ,response) => {
+    console.log(resquest.query.ordem)
+    return response.send("Saudações Cavaleiro!")
+})
+
+servidor.get("/Mago", (resquest ,response) => {
+    return response.send("Saudações Mago!")
+})
+
+
+servidor.get("/Arqueiro", (resquest ,response) => {
+    return response.send("Saudações Arqueiro!")
+})
+
+
+servidor.listen(3000)
